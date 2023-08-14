0fe0bfb4eb6171ff02a97b41c9c6b475f6b1688c
Dariopach <dariopach@hotmail.com>
2023-08-14 19:29:00 -0300

    version 2

--- src/app.js
+++ src/app.js
@@ -2,36 +2,38 @@ import express from 'express';
 import http from 'http';
 import { Server as SocketIOServer } from 'socket.io';
 import exphbs from 'express-handlebars';
-import { readFileSync, writeFileSync } from 'node:fs';
-import { ProductManager } from './productManager';
+import path from 'path';
+import __dirname from './utils.js';
+import { ProductManager } from './productManager.js';
 
 const app = express();
 const server = http.createServer(app);
 const io = new SocketIOServer(server);
 
+// Configurar Handlebars como el motor de plantillas
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public'))); 
 
-const productManager = new ProductManager('products.json'); // Cambiar por la ruta correcta
+const productManager = new ProductManager(path.join(__dirname, 'products.json')); 
 
 app.get('/', (req, res) => {
-  res.render('home', { products: productManager.getProducts() });
+  res.render('home', { products: ProductManager.getProducts() });
 });
 
 app.get('/realtimeproducts', (req, res) => {
-  res.render('realTimeProducts', { products: productManager.getProducts() });
+  res.render('realTimeProducts', { products: ProductManager.getProducts() });
 });
 
 io.on('connection', (socket) => {
   console.log('Usuario conectado');
 
   socket.on('createProduct', (product) => {
-    productManager.addProduct(product);
+    ProductManager.addProduct(product);
 
     // Emitir el evento 'updateProducts' a través de socket.io con la lista de productos actualizada
-    io.emit('updateProducts', productManager.getProducts());
+    io.emit('updateProducts', ProductManager.getProducts());
   });
 
   socket.on('disconnect', () => {
---
+++ src/routes/views.router.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { ProductManager } from '../productManager';
+
+const viewsRouter = express.Router();
+
+// Ruta para la vista de inicio
+viewsRouter.get('/', (req, res) => {
+  res.render('home', { products: ProductManager.getProducts() });
+});
+
+// Ruta para la vista en tiempo real de productos
+viewsRouter.get('/realtimeproducts', (req, res) => {
+  res.render('realTimeProducts', { products: ProductManager.getProducts() });
+});
+
+export default viewsRouter;
